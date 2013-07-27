ce415f75ea746188a7ffa135c32c546b7ab23fff
Kevin Sjöberg <kev.sjoberg@gmail.com>
2013-07-27 09:19:55 +0200

    Refactor user login feature

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -2,19 +2,17 @@ require 'spec_helper'
 
 describe "User logins", :type => :feature do
   context "with valid credentials" do
-    before do
-      @user = create(:user, 
+    let!(:user) { create(:user, 
                        email: 'user@example.com', 
                        password: 'password1',
-                       password_confirmation: 'password1')
-    end
+                       password_confirmation: 'password1') }
 
     it "logins successfully" do
       visit login_path
 
       within("#login-form") do
-        fill_in 'Email',    :with => @user.email
-        fill_in 'Password', :with => @user.password
+        fill_in 'Email',    :with => user.email
+        fill_in 'Password', :with => user.password
       end
 
       click_on "Login"
